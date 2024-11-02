6c232fa241afd961dcca7a7cef0157496bfb3e78
Truong_kenda <nguyenxuantruong2626@gmail.com>
2024-11-02 17:24:02 +0700

    if-eles

---
+++ src/FirstClass.java
@@ -0,0 +1,9 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello_world");
+
+        boolean isAilien =false;
+        if(isAilien==false);
+        System.out.println("It not an Ailien");
+    }
+}
